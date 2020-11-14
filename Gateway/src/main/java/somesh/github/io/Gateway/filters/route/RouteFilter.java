403a1e2423e4d0b9c4189205e12405721ec9fecc
Someshbose <iamsomeshbose@gmail.com>
2020-11-14 12:09:37 +0530

    refactoring of code

--- Gateway/src/main/java/somesh/github/io/Gateway/filters/route/RouteFilter.java
+++ Gateway/src/main/java/somesh/github/io/Gateway/filters/route/RouteFilter.java
@@ -1,27 +1,32 @@
 package somesh.github.io.Gateway.filters.route;
 
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.stereotype.Component;
 import com.netflix.zuul.ZuulFilter;
+import lombok.extern.slf4j.Slf4j;
 
+@Component
+@Slf4j
 public class RouteFilter extends ZuulFilter {
-  
+
   @Override
   public String filterType() {
-    return "route";
+    return FilterConstants.ROUTE_TYPE;
   }
- 
+
   @Override
   public int filterOrder() {
     return 1;
   }
- 
+
   @Override
   public boolean shouldFilter() {
     return true;
   }
- 
+
   @Override
   public Object run() {
-   System.out.println("Inside Route Filter");
+    log.info("Inside Route Filter");
     return null;
   }
 }
