12b39cd7cbb4a8cc1aa835de8b8655e3d7910546
Someshbose <iamsomeshbose@gmail.com>
2020-11-27 14:38:01 +0530

    infra changes added

--- Gateway/src/main/java/somesh/github/io/Gateway/GatewayApplication.java
+++ Gateway/src/main/java/somesh/github/io/Gateway/GatewayApplication.java
@@ -5,11 +5,23 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import lombok.extern.slf4j.Slf4j;
 
+/**
+ * Gateway application main class.
+ * 
+ * @author sombose
+ *
+ */
 @Slf4j
 @EnableZuulProxy
 @SpringBootApplication
+@SuppressWarnings("HideUtilityClassConstructor")
 public class GatewayApplication {
 
+  /**
+   * main method.
+   * 
+   * @param args String[]
+   */
   public static void main(String[] args) {
     SpringApplication.run(GatewayApplication.class, args);
     log.info("Gateway Application is started.");
